ebe1d53924b3ee72b32634009e7e2875538ceaba
MEMozki <memozki@bk.ru>
2025-01-04 17:22:48 +0300

    Add files via upload

---
+++ Classes/Packets/Client/Friend/AskForAddableFriendsMessage.py
@@ -0,0 +1,32 @@
+from Classes.ClientsManager import ClientsManager
+from Classes.Packets.PiranhaMessage import PiranhaMessage
+from Classes.Packets.Server.Home.BattleLogMessage import BattleLogMessage
+from Classes.Messaging import Messaging
+
+from Classes.ByteStream import ByteStream
+
+
+class AskForAddableFriendsMessage(PiranhaMessage):
+    def __init__(self, messageData):
+        super().__init__(messageData)
+        self.messageVersion = 0
+        
+
+    def decode(self):
+        fields = {}
+        fields["Unk1"] = self.readVInt()
+        fields["Unk2"] = self.readVInt()
+        return fields
+
+    def encode(self):
+    	pass
+    
+    def execute(message, calling_instance, fields):
+        fields["Socket"] = calling_instance.client
+        Messaging.sendMessage(20107, fields, calling_instance.player)
+
+    def getMessageType(self):
+        return 10503
+
+    def getMessageVersion(self):
+        return self.messageVersion
